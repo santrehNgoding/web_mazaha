6790de4de06a4cd192b4565b95e070d32ccc14e6
zahra.py <azzahrasidqiana22@gmail.com>
2025-02-20 15:52:32 +0700

    Add files via upload

---
+++ HTML/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [App\Http\Controllers\BerandaController::class, 'index'])->name('beranda');
+Route::get('/test', [App\Http\Controllers\BerandaController::class, 'test'])->name('test');
+Auth::routes();
+Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/beranda', [App\Http\Controllers\BerandaController::class, 'index'])->name('beranda');
+Route::get('/uji', [App\Http\Controllers\halamancontroller::class, 'uji'])->name('uji');
+Route::get('/uji2', [App\Http\Controllers\halamancontroller::class, 'uji2'])->name('uji2');
+Route::get('/hallo', [App\Http\Controllers\HalloController::class, 'index'])->name('hallo');
+Route::get('/history', [App\Http\Controllers\HistoryController::class, 'history'])->name('history');
+Route::get('/lihat', [App\Http\Controllers\halamanController::class, 'lihat'])->name('lihat');
+Route::get('/ujian', [App\Http\Controllers\halamancontroller::class, 'ujian'])->name('ujian');
+Route::get('/result', [App\Http\Controllers\Hasilcontroller::class, 'result'])->name('result');
+Route::resource('mapel', App\Http\Controllers\MataPelajaranController::class);
+Route::resource('coba', App\Http\Controllers\CobaController::class);
+Route::resource('users', App\Http\Controllers\UserController::class)->middleware('auth');
+Route::resource('jurusan', App\Http\Controllers\JurusanController::class);
+Route::resource('testimoni', App\Http\Controllers\TestimoniController::class);
+Route::resource('courses', App\Http\Controllers\CoursesController::class);
+Route::resource('campus', App\Http\Controllers\CampusController::class);
+Route::resource('tesuji', App\Http\Controllers\TesUjiController::class)->middleware('auth');
+use App\Http\Controllers\EksporController;
+Route::get('/ekspor-pdf', [EksporController::class, 'ekspor'])->name('ekspor-pdf');
+Route::get('/tesuji/detresult/{id}', [App\Http\Controllers\TesUjiController::class, 'detresult'])->name('tesuji.detresult');
+Route::get('/tesuji/dethistory/{id}', [App\Http\Controllers\TesUjiController::class, 'dethistory'])->name('tesuji.dethistory');
+Route::get('/tesuji/editPilgan/{id}', [App\Http\Controllers\TesUjiController::class, 'editPilgan'])->name('tesuji.editPilgan');
+use App\Http\Controllers\UserController;
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::post('/users/{id}/deleteProfile', [UserController::class, 'deleteProfile'])->name('users.deleteProfile');
+
+use App\Http\Controllers\TesUjiController;
+Route::get('/tesuji/reset', [TesUjiController::class, 'reset'])->name('tesuji.reset');
+Route::get('hasil', [TesUjiController::class, 'hasil'])->name('hasil');
+Route::put('/tesuji/updatePilgan/{id}', [TesUjiController::class, 'updatePilgan'])->name('tesuji.updatePilgan');
+
+
+
+
