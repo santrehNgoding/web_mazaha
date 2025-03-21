362e13c9b57ecbb30098aeea94c3dad962921dec
zahra.py <azzahrasidqiana22@gmail.com>
2025-03-21 10:30:29 +0700

    Create test.blade.php

---
+++ views/layouts/test.blade.php
@@ -0,0 +1,64 @@
+<div class="all">
+    <h3 style="color:#333;font-weight:400;margin-top:30px;margin-left:10px">Testimoni Alumni</h3>
+        <div class="slider-container">
+            <div class="slides">
+                <div class="slide">
+                    <div class="testimonial-card">
+                        <div class="testimonial-card2">
+                        <img src="/img/profil-20241106 075549.jpg" style="object-fit:cover;object-position:top" alt="Foto Alumni" class="profile-pic">
+                        <div class="text-content">
+                            <h5 style="font-weight: 700">Tiara Andini</h5>
+                            <p>Presiden Mahasiswa Umum</p>
+                            </div>
+                        </div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum blanditiis illo hic dicta id quisquam consequuntur reiciendis magni itaque labore officiis aspernatur facilis optio dolorum ipsa earum nulla, eaque tempora incidunt, quo veritatis. Repellendus incidunt exercitationem quo inventore corrupti provident pariatur libero cumque, ratione dolor vitae debitis autem eaque at.</p>
+                    </div>
+                </div>
+                <div class="slide">
+
+                        <div class="testimonial-card">
+                            <div class="testimonial-card2">
+                            <img src="/img/profil-20241106 075549.jpg" style="object-fit:cover;object-position:top" alt="Foto Alumni" class="profile-pic">
+                            <div class="text-content">
+                                <h5  style="font-weight: 700">Tiara Andini</h5>
+                                <p>Presiden Mahasiswa Umum</p>
+                                </div>
+                            </div><p>"Luar biasa! Saya sangat menikmati pengalaman belajar di sini."</p>
+                        </div>
+                </div>
+                <div class="slide">
+                    <div class="testimonial-card">
+                        <div class="testimonial-card2">
+                        <img src="/img/profil-20241106 075549.jpg" style="object-fit:cover;object-position:top" alt="Foto Alumni" class="profile-pic">
+                        <div class="text-content">
+                            <h5  style="font-weight: 700">Tiara Andini</h5>
+                            <p>Presiden Mahasiswa Umum</p>
+                            </div>
+                        </div><p>"Luar biasa! Saya sangat menikmati pengalaman belajar di sini."</p>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="testimonial-card">
+                        <div class="testimonial-card2">
+                        <img src="/img/profil-20241106 075549.jpg" style="object-fit:cover;object-position:top" alt="Foto Alumni" class="profile-pic">
+                        <div class="text-content">
+                            <h5  style="font-weight: 700">Tiara Andini</h5>
+                            <p>Presiden Mahasiswa Umum</p>
+                            </div>
+                        </div><p>"Luar biasa! Saya sangat menikmati pengalaman belajar di sini."</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="slider-controls">
+            
+            <div class="dots">
+                <button class="prev" onclick="moveSlide(-1)">&#10094;</button>
+                <span class="dot" onclick="moveSlideTo(0)"></span>
+                <span class="dot" onclick="moveSlideTo(1)"></span>
+                <span class="dot" onclick="moveSlideTo(2)"></span>
+                <button class="next" onclick="moveSlide(1)">&#10095;</button>
+
+            </div>
+        </div>
+    </div> 
