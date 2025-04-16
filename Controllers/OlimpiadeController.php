e0cf44df2b43642bafdd19b2bbe09a7a21bb5b9b
Zahra Coderss <azzahrasidqiana22@gmail.com>
2025-04-17 01:16:03 +0700

    Add files via upload

---
+++ Controllers/OlimpiadeController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class OlimpiadeController extends Controller
+{
+public function index()
+{
+    $rows=\App\Models\PrestasiOlimpiade::orderBy('created_at','desc')->get();
+    $teachers=\App\Models\GuruOlimpiade::all();
+    return view('olimpiade.index',['rows'=>$rows,'teachers'=>$teachers]);
+}
+public function create()
+{
+    return view('prestasi.create');
+}
+public function store(request $request)
+{
+    $file = $request->file('files');
+        $ekstensi = $file->getClientOriginalExtension();
+        $tanggal = str_replace(['-',':',''],'',\Carbon\Carbon::now());
+        $namaFile = 'winner-'.$tanggal.'.'.$ekstensi;
+        $file->move(public_path('img/prestasi'), $namaFile);
+        \App\Models\Prestasi::create([
+            'title'=>$request['title'],
+            'file'=>$namaFile,
+            'tahun'=>$request['tahun'],
+            'berita'=>$request['berita'],
+            'created_at'=>$request['created_at'],
+        ]);
+        return redirect()->to('/ekstra')->with('success','berhasil');
+}   
+public function edit($id){
+    $row = \App\Models\Prestasi::find($id);
+
+    if (!$row) {
+        return redirect()->route('ekstra.index')->with('error', 'Data tidak ditemukan.');
+    }
+
+    return view('prestasi.update', compact('row'));
+}
+
+    public function update(request $request, $id)
+    {  
+             if($request->hasFile('files')) {
+            $file = $request->file('files');
+            $ekstensi = $file->getClientOriginalExtension();
+            $tanggal = str_replace(['-',':',''],'',\Carbon\Carbon::now());
+            $namaFile = 'winner-'.$tanggal.'.'.$ekstensi;
+            $file->move(public_path('img/prestasi'), $namaFile);
+            $data['files'] = $namaFile;
+           } else{
+            $foto=\App\Models\Prestasi::where('id',$id)->first();
+            $namaFile=$foto['file'];
+           }
+           $data=[
+
+               'title'=>$request['title'],
+               'file'=>$namaFile,
+               'tahun'=>$request['tahun'],
+               'berita'=>$request['berita'],
+               'created_at'=>$request['created_at'],
+           ];
+           \App\Models\Prestasi::where('id', $id)->update($data);
+           
+           return redirect()->to('/ekstra')->with('success','berhasil');
+
+    }
+    
+}
