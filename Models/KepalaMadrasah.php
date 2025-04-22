c835f00ff1af691e4e3b5c41b5f2b72a49807bed
Zahra Coderss <azzahrasidqiana22@gmail.com>
2025-04-22 07:52:49 +0700

    Create KepalaMadrasah.php

---
+++ Models/KepalaMadrasah.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class KepalaMadrasah extends Model
+{
+    public $timestamps = false;
+    protected $table = 'kepala_madrasah';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id',
+        'nama',
+        'foto',
+        'tahun_mulai',
+        'tahun_selesai',
+        'profil',
+        'gagasan',
+    ];
+}
